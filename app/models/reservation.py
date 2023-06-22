c70cced2aa67a6c8b5d18524a7919e14f073cf03
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-06-22 17:44:58 -0500

    reservation model started

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -1,21 +1,39 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
-from sqlalchemy import Enum
+# from sqlalchemy import Enum
 
-class ReservationStatus(Enum):
-    CONFIRMED = "confirmed"
-    CANCELLED = "cancelled"
-    ATTENDED = "attended"
+# class ReservationStatus(Enum):
+#     CONFIRMED = "confirmed"
+#     CANCELLED = "cancelled"
+#     ATTENDED = "attended"
 
 
 class Reservation(db.Model):
+    __tablename__ = 'reservations'
+    
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('restaurants.id')), nullable=False)
     number_of_people = db.Column(db.Integer, nullable=False)
     reservation_time = db.Column(db.DateTime, nullable=False)
-    status = db.Column(db.Enum(ReservationStatus), nullable=False)
-    # status = db.Column(db.String(50), nullable=False)
+    # status = db.Column(db.Enum(ReservationStatus), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
     notes = db.Column(db.String(255), nullable=True)
     created_at = db.Column(db.DateTime, default= datetime.utcnow)
     updated_at = db.Column(db.DateTime, default= datetime.utcnow, onupdate=datetime.utcnow)
+
+    restaurant_customers = db.relationship('User', back_populates='reservations')
+    restaurant_reservations = db.relationship('Restaurant', back_populates='reservations')
+
+def to_dict(self):
+    return {
+        'id': self.id,
+        'userId': self.user_id,
+        'restaurantId': self.restaurant_id,
+        'numberOfPeople': self.number_of_people,
+        'reservationTime': self.reservation_time,
+        'status': self.status,
+        'notes': self.notes,
+        'createdAt': self.created_at,
+        'updatedAt': self.updated_at,
+    }
