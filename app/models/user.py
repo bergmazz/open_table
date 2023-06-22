40b23d54f4913aa4d59d5ea4b45655eee718aa26
bergmazz <ebergmann@uarts.edu>
2023-06-21 20:15:14 -0400

    adding columns to user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -10,9 +11,16 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    firstName = db.Column(db.String(40), nullable=False, unique=True)
+    lastName = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    phoneNumber = db.Column(db.Integer, nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    owner = db.Column(db.Boolean, default=False, unique=True)
+    created_at = db.Column(db.DateTime, default= datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default= datetime.utcnow, onupdate=datetime.utcnow)
+
+    # following = db.relationship('Follower', back_populates='follower_user', foreign_keys='Follower.follower_id')
 
     @property
     def password(self):
@@ -28,6 +36,12 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'firstName': self.first_name,
+            'lastName': self.last_name,
+            'email': self.email,
+            "phoneNumber": self.phone_number,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at,
+            # 'followers': [follower.to_dict() for follower in self.followers],
+            #  'numFollowers': len(self.followers)
         }
