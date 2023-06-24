3922753560b451cdc52e73fc8b0cb26528074381
bergmazz <ebergmann@uarts.edu>
2023-06-24 14:27:17 -0400

    user seeds

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,15 +5,96 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        first_name='Demo',
+        last_name='User',
+        email='demo@aa.io',
+        phone_number='1234567890',
+        password='password',
+        owner=True
+    )
+    gordon = User(
+        first_name='Gordon',
+        last_name='Ramsay',
+        email='gordon@idiotsandwich.com',
+        phone_number='1223456789',
+        password='password',
+        owner=True
+    )
+    action = User(
+        first_name='Action',
+        last_name='Bronson',
+        email='misterbaklava@bambam.io',
+        phone_number='5555555555',
+        password='password',
+        owner=True
+    )
+    martha = User(
+        first_name='Martha',
+        last_name='Stewart',
+        email='mdiddy@bic-ez-reach.com',
+        phone_number='5555555556',
+        password='password',
+        owner=True
+    )
+    sean = User(
+        first_name='Sean',
+        last_name='Evans',
+        email='hotones@firstwefeast.com',
+        phone_number='5555555557',
+        password='password',
+        owner=True
+    )
+    babbish = User(
+        first_name='Andrew',
+        last_name='Rea',
+        email='bingingwith@babbish.com',
+        phone_number='5555555558',
+        password='password',
+        owner=False
+    )
+    dope = User(
+        first_name='Nicole',
+        last_name='DiMascio',
+        email='nic@dopekitchen.com',
+        phone_number='5555555559',
+        password='password',
+        owner=False
+    )
+    cakeboss = User(
+        first_name='Buddy',
+        last_name='Valastro',
+        email='bud@cakeboss.com',
+        phone_number='5555555560',
+        password='password',
+        owner=False
+    )
+    guy = User(
+        first_name='Guy',
+        last_name='Fieri',
+        email='guy@flavortown.com',
+        phone_number='5555555561',
+        password='password',
+        owner=False
+    )
+    dom = User(
+        first_name='Dominique',
+        last_name='Crenn',
+        email='chef@ateliercrenn.com',
+        phone_number='5555555562',
+        password='password',
+        owner=False
+    )
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(gordon)
+    db.session.add(martha)
+    db.session.add(action)
+    db.session.add(sean)
+    db.session.add(babbish)
+    db.session.add(dope)
+    db.session.add(cakeboss)
+    db.session.add(guy)
+    db.session.add(dom)
     db.session.commit()
 
 
@@ -28,5 +109,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
