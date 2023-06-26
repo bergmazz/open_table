064113dcbc6d3f0dc2687c63d4b48dcfc8440e7a
lola <keepthechange2021@gmail.com>
2023-06-26 12:24:31 -0700

    added error message to get all restaurants reviews

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -4,12 +4,17 @@ from app.models import Review, Restaurant
 
 review_routes = Blueprint('restaurants/<int:restaurant_id>/reviews', __name__)
 
-# Get all Reviews
+# Get all Reviews by Restaurant id
 @review_routes.route('/', methods=['GET'])
 def get_restaurant_reviews(restaurant_id):
     """
     Gets a list the restaurant's reviews
 
     """
+    if Restaurant.query.get(restaurant_id) is None:
+        return jsonify({'error': 'Restaurant not found'}), 404
+
     reviews = [review.to_dict() for review in Review.query.filter_by(restaurant_id=restaurant_id).all()]
+
     return jsonify({ "reviews": reviews }), 200
+
