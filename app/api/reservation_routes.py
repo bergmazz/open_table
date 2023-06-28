36407cec6c5f212903b17d57a581a04f5a80e1ad
lola <keepthechange2021@gmail.com>
2023-06-28 09:00:04 -0700

    finished create and edit reservation

--- app/api/reservation_routes.py
+++ app/api/reservation_routes.py
@@ -51,6 +51,9 @@ def edit_reservation(restaurant_id, reservation_id):
 
     reservation = Reservation.query.get(reservation_id)
 
+    if reservation is None:
+        return jsonify({'error': 'Reservation not found'}), 404
+
     if current_user.id is not reservation.user_id:
         return jsonify({ 'error': 'You are not authorized to edit this post' })
 
@@ -65,37 +68,3 @@ def edit_reservation(restaurant_id, reservation_id):
         for field_name, field_errors in form.errors.items():
             errors[field_name] = field_errors[0]
         return {'error': errors}
-
-
-
-# Delete a Reservation (technically just updates status to cancel)
-# @reservation_routes.route('/<int:reservation_id>', methods=['PUT'])
-# @login_required
-# def delete_reservation(reservation_id, restaurant_id):
-#     """
-#     Cancels a reservation
-#     Does not delete from database so restaurant owners can keep track of canceled slots
-#     Only the restuarant owner OR user who booked the reservation can delete
-#     """
-#     reservation = Reservation.query.get(reservation_id)
-#     restaurant = Restaurant.query.get(restaurant_id)
-
-#     if restaurant is None:
-#         return jsonify({'error': 'Restaurant not found'}), 404
-#     if reservation is None:
-#         return jsonify({'error': 'Reservation not found'}), 404
-
-#     if current_user.id not in [reservation.user_id, restaurant.user_id]:
-#             return jsonify({ 'error': 'You are not authorized to cancel this reservation' })
-
-#     if datetime.utcnow > reservation.reservation_time:
-#         if reservation.status is not "cancelled":
-#             reservation.status = "attended"
-
-#     if reservation.status is not "confirmed":
-#         return jsonify({'error': 'Reservation has already been cancelled or attended'}), 404
-
-#     reservation.status = "cancelled"
-#     # db.session.delete(reservation)
-#     db.session.commit()
-#     return {'message': 'Reservation successfully cancelled'}
--- app/forms/reservation_form.py
+++ app/forms/reservation_form.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, DateTimeField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 from app.models import Reservation
+import datetime
+
+def validate_date(form, field):
+    if field.data.date() < datetime.date.today():
+        raise ValidationError("reservation time cannot be in the past")
 
 class ReservationForm(FlaskForm):
     number_of_people = IntegerField('Number of People', validators=[DataRequired()])
-    reservation_time = DateTimeField('Reservation Time', validators=[DataRequired()])
+    reservation_time = DateTimeField('Reservation Time', validators=[DataRequired(), validate_date])
     status = SelectField("Status", choices=[("Confirmed", "confirmed"), ("Attended", "attended"), ("Cancelled", "cancelled")])
     notes = StringField('Notes')
