29506fefc47bb28ee3440c5337dfeeee95efbe6d
bergmazz <ebergmann@uarts.edu>
2023-06-29 19:03:09 -0400

    resolved seed order issue

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,22 +19,21 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
         undo_reviews()
-        undo_restaurants()
         undo_reservations()
+        undo_restaurants()
+        undo_users()
     seed_users()
+    seed_restaurants()
     seed_reservations()
     seed_reviews()
-    seed_restaurants()
-    # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_reservations()
     undo_reviews()
+    undo_reservations()
     undo_restaurants()
+    undo_users()
     # Add other undo functions here
