add04ac5a9069092085568ed5012af688e5074f7
bergmazz <ebergmann@uarts.edu>
2023-06-29 19:16:32 -0400

    deleted migrations folder yet again

--- migrations/versions/f45987f3ffcc_.py
+++ migrations/versions/f45987f3ffcc_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 7060bae58551
+Revision ID: f45987f3ffcc
 Revises: 
-Create Date: 2023-06-29 18:36:41.367256
+Create Date: 2023-06-29 19:14:30.070110
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7060bae58551'
+revision = 'f45987f3ffcc'
 down_revision = None
 branch_labels = None
 depends_on = None
