4af57e1337c8714fbbcdfa036a9171dd3a792aad
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-06-29 20:17:46 -0500

    created homepage

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,6 +6,7 @@ import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import AllRestaurants from "./components/Restaurants/AllRestaurants";
+import HomePage from "./components/HomePage";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,6 +20,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
           <Route path="/login" >
             <LoginFormPage />
           </Route>
---
+++ react-app/src/components/HomePage/index.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Discover a table for every event</h1>
+        </div>
+    )
+}
+
+export default HomePage;
