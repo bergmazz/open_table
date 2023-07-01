52913ac075d777c022a2f7cfb60b4c80474f3b56
lola <keepthechange2021@gmail.com>
2023-07-01 11:57:28 -0700

    restaurant page nav bar

--- react-app/src/components/Restaurants/RestaurantPage/index.js
+++ react-app/src/components/Restaurants/RestaurantPage/index.js
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { getDetailsRestaurant } from "../../../store/restaurantDetails";
+import './RestaurantPage.css'
 
 const RestaurantPage = () => {
     const dispatch = useDispatch();
@@ -22,16 +23,36 @@ const RestaurantPage = () => {
     if (Object.keys(restaurant).length) {
         console.log("RESTAURANTAASSSSSSSSSSSSSSS", restaurant)
         return (
-            <>
-                <h1>Restaurant Details</h1>
-                <div>Restaurant Name: {`${restaurant.restaurantName}`}</div>
-                <h2>Reviews</h2>
-                {
-                    restaurant.reviews.map(review => (
-                        <p>{`${review.comment}`}</p>
+            <div className="restaurant-container">
+
+                <div className="restaurant-cover-image">
+                        <img style={{ width: "100%", height: "100%"}} src={`${restaurant.coverImage}`} alt="" />
+                </div>
+
+                <div className="restaurant-column1">
+                    <ul className="restaurant-navbar">
+                        <li>
+                            <NavLink exact to ="/restaurants/`${restaurant.id}`">Overview</NavLink>
+                        </li>
+                        <li>
+                            <a id="restaurant-reviews" href="#element_target">Reviews</a>
+                        </li>
+                    </ul>
+                    <h1>{`${restaurant.restaurantName}`}</h1>
+                    <div id="restaurant-reviews">
+                    {
+                        restaurant.reviews.map(review => (
+                            <p>{`${review.comment}`}</p>
                     ))
-                }
-            </>
+                    }
+                    </div>
+                </div>
+
+                <div className="restaurant-column2">
+                    <h2>Make a reservation</h2>
+                </div>
+
+            </div>
         )
     } else {
         return (
