e7a290cb8689c37c1100c79a50975179e5a77c08
lola <keepthechange2021@gmail.com>
2023-07-06 14:01:20 -0700

    working still

--- react-app/src/components/Restaurants/RestaurantPage/index.js
+++ react-app/src/components/Restaurants/RestaurantPage/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getDetailsRestaurant } from "../../../store/restaurantDetails";
 import './RestaurantPage.css'
 
@@ -29,6 +29,46 @@ const RestaurantPage = () => {
         if (restaurant.priceRange <= 2) priceRange = "$30 and under";
         if (restaurant.priceRange === 3) priceRange = "$31 to $50";
         const description = "Whether you’re chatting over a flavorful roasted chicken salad or sipping a merlot as your hand-cut blackened ribeye is mesquite grilled over a live fire, Paul Martin’s American Grill is the ideal place to impress or decompress.";
+
+        const fullStars = Math.floor(restaurant.averageRating);
+        // Gets the number of full stars. starAverage is the rating, for example
+        // if the rating were 4.3, fullStars would now be 4.
+
+        const starArr = [];
+        // Create an empty array. We will add 1s, 0s, and a decimal value for the
+        // partial star.
+
+        for (let i = 1; i <= fullStars; i++) {
+            starArr.push(1);
+        }
+        // This adds a 1 to the array for each full star in our rating
+
+        if (restaurant.averageRating < 5) {
+            // Wrapped in an if block because the following only needs to occur if
+            // it's not a full 5.
+
+            const partialStar = restaurant.averageRating - fullStars;
+            // Calculates the partial star. For example 4.3 - 4 = 0.3. 0.3 will get
+            // added to the array in the next line to represent the partial star
+
+            starArr.push(partialStar);
+            // Adds the partial star to the array
+
+            const emptyStars = 5 - starArr.length;
+            // Calculates the number of empty stars
+
+            for (let i = 1; i <= emptyStars; i++) {
+                starArr.push(0);
+            }
+            // This for loop adds 0s to the array to represent empty stars
+            console.log("starrrs", starArr)
+            function starRating(star) {
+                if (star === 0) return <i className="fa-regular fa-star" style="color: #d50b1f;"></i>
+                else if (star < 1) return <i className="fa-solid fa-star-half-stroke" style="color: #d50b1f;"></i>
+                else return <i className="fa-solid fa-star" style="color: #d50b1f;"></i>
+            }
+        }
+
         return (
             <div className="outer-restaurant-container">
                 <div className="restaurant-container">
@@ -51,17 +91,25 @@ const RestaurantPage = () => {
                         <div className="random-box">
                             <div className="ratings-average-bar">
                                 <div className="ratings">
-                                    <meter class="average-rating" min="0" max="5" value={restaurant.averageRating}></meter>
-                                    <span className="number-rating">{restaurant.averageRating}</span>
+                                    <div className="star-row">
+                                    {
+                                        starArr.map(star => {
+                                            if (star === 0) return <i className="fa-regular fa-star"></i>
+                                            else if (star < 1) return <i className="fa-solid fa-star-half-stroke" ></i>
+                                            else return <i className="fa-solid fa-star" ></i>
+                                        })
+                                    }
+                                    </div>
+                                    <div className="number-rating">{restaurant.averageRating}</div>
                                 </div>
                                 <div className="review-summary">
                                     <i className="fa-regular fa-message"></i>
                                     <span>  {restaurant.reviews.length} {reviews}</span>
                                 </div>
                                 <div className="money">
-                                    <i class="fa-sharp fa-regular fa-money-bill-1"></i> {priceRange}
+                                    <i className="fa-sharp fa-regular fa-money-bill-1"></i> {priceRange}
                                 </div>
-                                <div className="cuisine-type"><i class="fa-solid fa-utensils"></i> {restaurant.cuisineType}</div>
+                                <div className="cuisine-type"><i className="fa-solid fa-utensils"></i> {restaurant.cuisineType}</div>
 
                             </div>
                             {/* <div className="top-tags"></div>
@@ -73,16 +121,16 @@ const RestaurantPage = () => {
                                 <div className="sort-reviews">Newest</div>
                             </div>
                             <div className="reviews-area">
-                            {
-                                restaurant.reviews.map(review => (
-                                    <div className="individual-review">
-                                        <div className="user-section">User</div>
-                                        <div className="review-section">
-                                        {review.comment}
+                                {
+                                    restaurant.reviews.map((review, i) => (
+                                        <div className="individual-review" key={i}>
+                                            <div className="user-section">User</div>
+                                            <div className="review-section">
+                                                {review.comment}
+                                            </div>
                                         </div>
-                                    </div>
-                                ))
-                            }
+                                    ))
+                                }
                             </div>
                         </div>
                     </div>
