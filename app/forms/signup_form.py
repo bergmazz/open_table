3a47b5f1a7210700aaca56bc4bb53922b02db66b
bergmazz <ebergmann@uarts.edu>
2023-07-16 23:44:01 -0400

    backend validations for valid area codes on phone numbers in user and restaurant

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -20,6 +20,16 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def phone_number_valid(form, field):
+    #Don't allow leading zeros or ones
+    phone_number = field.data
+    area_code = int(phone_number[:3])
+    # print("---------------phone:", phone_number)
+    # print("---------------area:",area_code)
+    if area_code < 200:
+        raise ValidationError('Please enter a valid US phone number, starting with an area code between 2 and 9.')
+
+
 def phone_number_format(form, field):
     #Checking if phone number is in correct format and contains 10 digits
     phone_number = field.data
@@ -32,6 +42,6 @@ class SignUpForm(FlaskForm):
     first_name = StringField('first name', validators=[DataRequired()])
     last_name = StringField('last name', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(), Email(), user_exists])
-    phone_number = StringField('phone number', validators=[DataRequired(message="Phone number must be digits only"), phone_number_format])
+    phone_number = StringField('phone number', validators=[DataRequired(message="Phone number must be digits only"), phone_number_format, phone_number_valid])
     password = StringField('password', validators=[DataRequired()])
     owner = BooleanField('owner')
